ec007bb51f232ee8c245da0a0b9d884022af75e2
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-11 19:38:17 +0700

    tampilan-dashboard

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,9 +1,18 @@
-export function load({ cookies }) {
-	const user = cookies.get('user');
+import { db } from '$lib/server/db';
+import { users } from '$lib/server/db/schema.js';
+import { redirect } from '@sveltejs/kit';
 
-	console.log('COOKIE:', user);
+export async function load({ cookies }) {
+	const user = cookies.get('user');
 
 	if (!user) {
 		throw redirect(303, '/login');
 	}
+
+	// ambil semua user
+	const allUsers = await db.select().from(users);
+
+	return {
+		users: allUsers
+	};
 }
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,9 +1,30 @@
-<form method="POST" action="/logout">
-	<button class="bg-red-500 text-white px-4 py-2 rounded mt-5">
-		Logout
-	</button>
-</form>
+<script>
+	export let data;
+</script>
 
-<h1 class="text-3xl font-bold text-center mt-10">
-	Dashboard 🎉
-</h1>
+<h1 class="text-2xl font-bold mb-5">Dashboard</h1>
+
+<table class="w-full border">
+	<tr class="bg-gray-200">
+		<th>ID</th>
+		<th>Username</th>
+		<th>Alamat</th>
+		<th>Aksi</th>
+	</tr>
+
+	{#each data.users as user}
+	<tr class="border">
+		<td>{user.id}</td>
+		<td>{user.username}</td>
+		<td>{user.alamat || '-'}</td>
+		<td>
+			<form method="POST" action="/dashboard/delete">
+				<input type="hidden" name="id" value={user.id} />
+				<button class="bg-red-500 text-white px-2 py-1 rounded">
+					Hapus
+				</button>
+			</form>
+		</td>
+	</tr>
+	{/each}
+</table>
