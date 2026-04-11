9d03b5c2ff4a0056a22ee9b1fa167c53f8c93b74
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-11 10:09:59 +0700

    dashboard

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<form method="POST" action="/logout">
+	<button class="bg-red-500 text-white px-4 py-2 rounded mt-5">
+		Logout
+	</button>
+</form>
+
+<h1 class="text-3xl font-bold text-center mt-10">
+	Dashboard 🎉
+</h1>
