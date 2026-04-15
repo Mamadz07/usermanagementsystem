60ecf974d508e8e3d699c89634786f4cb2fa3965
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-15 20:17:57 +0700

    percobaan4

---
+++ src/lib/server/schema.js
@@ -0,0 +1,9 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	username: text('username'),
+	password: text('password'),
+	alamat: text('alamat'),
+	foto: text('foto')
+});
