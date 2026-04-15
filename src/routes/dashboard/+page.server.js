a202f6d50e9f691262759a2e57de3843ee6c678d
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-15 20:34:44 +0700

    percobaan6

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,5 +1,5 @@
 import { db } from '$lib/server/db';
-import { users } from '$lib/server/db/schema';
+import { users } from '$lib/server/schema';
 import { eq } from 'drizzle-orm';
 import { redirect, fail } from '@sveltejs/kit';
 
